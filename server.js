1ea873a3a8fbf61ab3640abf7f5b6e0efca58c33
nodejs-group <bishnoipankaj448@gmail.com>
2022-05-12 09:24:51 +0530

    database connection

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE_URI.replace(
+  /<password>/,
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => console.log("Error connecting to database! \n " + err));
 
 app.listen(port, "localhost", () => {
   console.log("server is listening on at http://localhost:%d", port);
